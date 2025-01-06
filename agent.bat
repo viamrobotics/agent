4a98019861e814762b67f962ef86cebdb09fd335
Abe Winter <abe.winter@viam.com>
2025-01-06 02:43:37 -0500

    use vars in batch script

--- agent.bat
+++ agent.bat
@@ -1,10 +1,13 @@
 @echo off
 :: installer for agent on windows
 
-mkdir \opt\viam\cache
-mkdir \opt\viam\bin
-curl https://storage.googleapis.com/packages.viam.com/temp/viam-agent-windows-amd64-alpha-1-17bbf00.exe -o \opt\viam\cache\viam-agent-windows-amd64-alpha-1-17bbf00.exe
-del \opt\viam\bin\viam-agent.exe
-mklink \opt\viam\bin\viam-agent.exe \opt\viam\cache\viam-agent-windows-amd64-alpha-1-17bbf00.exe
-sc create viam-agent binpath= c:\opt\viam\bin\viam-agent.exe start= auto
+set root=\opt\viam
+set fname=viam-agent-windows-amd64-alpha-1-17bbf00.exe
+mkdir %root%\cache
+mkdir %root%\bin
+curl https://storage.googleapis.com/packages.viam.com/temp/%fname% -o %root%\cache\%fname%
+:: netsh %root%\cache\%fname%
+del %root%\bin\viam-agent.exe
+mklink %root%\bin\viam-agent.exe %root%\cache\%fname%
+sc create viam-agent binpath= c:%root%\bin\viam-agent.exe start= auto
 sc start viam-agent
