09913040b1abc7623aed3b816b91b1845af14d7a
Abe Winter <abe.winter@viam.com>
2025-01-10 17:34:39 -0500

    top-level firewall rule seems to be working

--- agent.bat
+++ agent.bat
@@ -6,7 +6,7 @@ set fname=viam-agent-windows-amd64-alpha-1-17bbf00.exe
 mkdir %root%\cache
 mkdir %root%\bin
 curl https://storage.googleapis.com/packages.viam.com/temp/%fname% -o %root%\cache\%fname%
-:: netsh %root%\cache\%fname%
+netsh advfirewall firewall add rule name="%fname%" dir=in action=allow program="c:\%root%\cache\%fname%" enable=yes
 del %root%\bin\viam-agent.exe
 mklink %root%\bin\viam-agent.exe %root%\cache\%fname%
 sc create viam-agent binpath= c:%root%\bin\viam-agent.exe start= auto
