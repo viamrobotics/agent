2b02f7509d817ed219a2205271bbafb778d27387
Abe Winter <abe-winter@users.noreply.github.com>
2025-01-10 20:51:37 -0500

    alpha 4

--- agent.bat
+++ agent.bat
@@ -2,7 +2,7 @@
 :: installer for agent on windows
 
 set root=\opt\viam
-set fname=viam-agent-windows-amd64-alpha-3-0991304.exe
+set fname=viam-agent-windows-amd64-alpha-4-6af4e0f.exe
 mkdir %root%\cache
 mkdir %root%\bin
 curl https://storage.googleapis.com/packages.viam.com/temp/%fname% -o %root%\cache\%fname%
