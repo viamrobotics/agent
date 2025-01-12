935c7789571564d8471527ddf728df40e1b0742d
Abe Winter <abe-winter@users.noreply.github.com>
2025-01-12 16:27:55 -0500

    alpha-9 in agent.bat

--- agent.bat
+++ agent.bat
@@ -2,12 +2,13 @@
 :: installer for agent on windows
 
 set root=\opt\viam
-set fname=viam-agent-windows-amd64-alpha-4-6af4e0f.exe
+set fname=viam-agent-windows-amd64-alpha-9-f68e150.exe
 mkdir %root%\cache
 mkdir %root%\bin
 curl https://storage.googleapis.com/packages.viam.com/temp/%fname% -o %root%\cache\%fname%
 netsh advfirewall firewall add rule name="%fname%" dir=in action=allow program="c:\%root%\cache\%fname%" enable=yes
 del %root%\bin\viam-agent.exe
 mklink %root%\bin\viam-agent.exe %root%\cache\%fname%
+:: todo: restart on error
 sc create viam-agent binpath= c:%root%\bin\viam-agent.exe start= auto
 sc start viam-agent
