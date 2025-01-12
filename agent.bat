24b60acbef6fe060b0909f765204b060afe6c5cf
Abe Winter <abe-winter@users.noreply.github.com>
2025-01-12 16:54:11 -0500

    bump agent.bat

--- agent.bat
+++ agent.bat
@@ -2,7 +2,7 @@
 :: installer for agent on windows
 
 set root=\opt\viam
-set fname=viam-agent-windows-amd64-alpha-9-f68e150.exe
+set fname=viam-agent-windows-amd64-alpha-11-4725f4c.exe
 mkdir %root%\cache
 mkdir %root%\bin
 curl https://storage.googleapis.com/packages.viam.com/temp/%fname% -o %root%\cache\%fname%
