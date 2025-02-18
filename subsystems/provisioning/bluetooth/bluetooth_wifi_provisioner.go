0356d47b02230ba2c6a3607f6861560f65e9656f
Max Horowitz <maxhorowitz@gmail.com>
2025-02-18 11:04:49 -0500

    Pass context one layer lower in call stack to properly protect against sending information to a closed channel.

--- subsystems/provisioning/bluetooth/bluetooth_wifi_provisioner.go
+++ subsystems/provisioning/bluetooth/bluetooth_wifi_provisioner.go
@@ -55,10 +55,7 @@ func (bwp *BluetoothWiFiProvisioner) Stop() error {
 
 // Update updates the list of networks that are advertised via bluetooth as available.
 func (bwp *BluetoothWiFiProvisioner) RefreshAvailableNetworks(ctx context.Context, awns *AvailableWiFiNetworks) error {
-	if ctx.Err() != nil {
-		return ctx.Err()
-	}
-	return bwp.writeAvailableNetworks(awns)
+	return bwp.writeAvailableNetworks(ctx, awns)
 }
 
 // WaitForCredentials returns credentials, the minimum required information to provision a robot and/or its WiFi.
@@ -117,7 +114,7 @@ func (bwp *BluetoothWiFiProvisioner) stopAdvertisingBLE() error {
 
 func (bwp *BluetoothWiFiProvisioner) enableAutoAcceptPairRequest() {}
 
-func (bwp *BluetoothWiFiProvisioner) writeAvailableNetworks(networks *AvailableWiFiNetworks) error {
+func (bwp *BluetoothWiFiProvisioner) writeAvailableNetworks(ctx context.Context, networks *AvailableWiFiNetworks) error {
 	return errors.New("TODO APP-7644: Add Linux-specific bluetooth calls for automatic pairing and read/write to BLE characteristics")
 }
 
