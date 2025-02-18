018fa5e2b7938328b73c7724efda21ada6ccf0da
Max Horowitz <maxhorowitz@gmail.com>
2025-02-18 12:37:37 -0500

    Remove code from this PR (should live in another layer of PR stack).

--- subsystems/provisioning/bluetooth/bluetooth_wifi_provisioner_utils.go
+++ subsystems/provisioning/bluetooth/bluetooth_wifi_provisioner_utils.go
@@ -3,12 +3,9 @@ package ble
 import (
 	"context"
 	"fmt"
-	"strings"
 	"time"
 
-	"github.com/godbus/dbus"
 	"github.com/pkg/errors"
-	"go.viam.com/rdk/logging"
 )
 
 // emptyBluetoothCharacteristicError represents the error which is raised when we attempt to read from an empty BLE characteristic.
@@ -50,44 +47,4 @@ func waitForBLEValue(
 		}
 		return v, nil
 	}
-}
-
-const (
-	BluezDBusService  = "org.bluez"
-	BluezAgentPath    = "/custom/agent"
-	BluezAgentManager = "org.bluez.AgentManager1"
-	BluezAgent        = "org.bluez.Agent1"
-)
-
-// trustDevice sets the device as trusted and connects to it.
-func trustDevice(logger logging.Logger, devicePath string) error {
-	conn, err := dbus.SystemBus()
-	if err != nil {
-		return fmt.Errorf("failed to connect to DBus: %w", err)
-	}
-
-	obj := conn.Object(BluezDBusService, dbus.ObjectPath(devicePath))
-
-	// Set Trusted = true
-	call := obj.Call("org.freedesktop.DBus.Properties.Set", 0,
-		"org.bluez.Device1", "Trusted", dbus.MakeVariant(true))
-	if call.Err != nil {
-		return fmt.Errorf("failed to set Trusted property: %w", call.Err)
-	}
-	logger.Info("device marked as trusted.")
-
-	return nil
-}
-
-// convertDBusPathToMAC converts a DBus object path to a Bluetooth MAC address.
-func convertDBusPathToMAC(path string) string {
-	parts := strings.Split(path, "/")
-	if len(parts) < 4 {
-		return ""
-	}
-
-	// Extract last part and convert underscores to colons
-	macPart := parts[len(parts)-1]
-	mac := strings.ReplaceAll(macPart, "_", ":")
-	return mac
 }
