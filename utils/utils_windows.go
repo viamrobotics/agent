a6e0b130965859aa631ca984bb80bf1872a7d273
Abe Winter <abe.winter@viam.com>
2025-03-10 20:08:35 -0400

    logging in KillTree

--- utils/utils_windows.go
+++ utils/utils_windows.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 
 	"go.viam.com/rdk/logging"
+	"golang.org/x/sys/windows/svc/eventlog"
 )
 
 func PlatformProcSettings(cmd *exec.Cmd) {}
@@ -17,7 +18,7 @@ func PlatformProcSettings(cmd *exec.Cmd) {}
 func PlatformKill(logger logging.Logger, cmd *exec.Cmd) {}
 
 // platform-specific UID check.
-func checkPathOwner(uid int, info fs.FileInfo) error {
+func checkPathOwner(_ int, _ fs.FileInfo) error {
 	// todo(windows)
 	return nil
 }
@@ -37,6 +38,8 @@ func SyncFS(syncPath string) error {
 
 // KillTree kills the process tree on windows (because other signaling doesn't work).
 func KillTree(pid int) error {
+	elog, _ := eventlog.Open("viam-agent")
+	// note: we're ignoring the log error because we want this to work
 	if pid == -1 {
 		pid = os.Getpid()
 	}
@@ -48,6 +51,9 @@ func KillTree(pid int) error {
 		// elog.Error(1, fmt.Sprintf("error getting child process for #%d, #%s", pid, err))
 	}
 	lines := strings.Split(string(output), "\r\n")
+	if elog != nil {
+		elog.Info(1, fmt.Sprintf("KillTree stopping %d children of pid %d", len(lines), pid))
+	}
 	for _, line := range lines[1:] {
 		if line == "" {
 			continue
@@ -55,15 +61,23 @@ func KillTree(pid int) error {
 		var childPID int
 		_, err := fmt.Sscan(line, &childPID)
 		if err != nil {
-			// elog.Error(1, fmt.Sprintf("not a valid childProcess line %s, #%s", line, err))
+			if elog != nil {
+				elog.Error(1, fmt.Sprintf("not a valid childProcess line %q, #%s", line, err))
+			}
 			continue
 		}
 		cmd = exec.Command("taskkill", "/F", "/T", "/PID", strconv.Itoa(childPID))
-		cmd.Run()
-		// err = cmd.Run()
-		// if err != nil {
-		// 	// elog.Error(1, fmt.Sprintf("error running taskkill #%s", err))
-		// }
+		err = cmd.Run()
+		if elog != nil {
+			if err != nil {
+				elog.Error(1, fmt.Sprintf("error running taskkill pid %d: #%s", childPID, err))
+			} else {
+				elog.Info(1, fmt.Sprintf("killed pid %d", childPID))
+			}
+		}
+	}
+	if elog != nil {
+		elog.Info(1, "KillTree finished")
 	}
 	return nil
 }
