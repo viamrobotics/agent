5d4b580a0653a6fc64145b44a3d8bbdb5b315ded
James Otting <james@viam.com>
2025-03-26 15:48:05 -0500

    Split OS specific build files

---
+++ subsystems/syscfg/syscfg_windows.go
@@ -0,0 +1,13 @@
+package syscfg
+
+import (
+	"context"
+
+	"github.com/viamrobotics/agent/subsystems"
+	"github.com/viamrobotics/agent/utils"
+	"go.viam.com/rdk/logging"
+)
+
+func NewSubsystem(_ context.Context, _ logging.Logger, _ utils.AgentConfig) subsystems.Subsystem {
+	return &subsystems.Dummy{}
+}
