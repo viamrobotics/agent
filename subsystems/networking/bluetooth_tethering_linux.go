ad29935c82490f012fde808d6d45da569013334d
James Otting <james@viam.com>
2025-06-27 16:29:34 -0500

    WIP: working with manual NM setup

--- subsystems/networking/bluetooth_tethering_linux.go
+++ subsystems/networking/bluetooth_tethering_linux.go
@@ -2,6 +2,8 @@ package networking
 
 import (
 	"fmt"
+	"slices"
+	"sync"
 	"time"
 
 	dbus "github.com/godbus/dbus/v5"
@@ -9,20 +11,110 @@ import (
 	"go.viam.com/rdk/logging"
 )
 
+var errPairingRejected = &dbus.Error{Name: "org.bluez.Error.Rejected"}
+
 type basicAgent struct {
-	conn   *dbus.Conn
-	logger logging.Logger
+	mu      sync.Mutex
+	conn    *dbus.Conn
+	logger  logging.Logger
+	trusted []string
 }
 
-func (b *basicAgent) RequestAuthorization(device dbus.ObjectPath) *dbus.Error {
+func (b *basicAgent) Cancel() *dbus.Error {
+	b.logger.Debug("SMURF Cancel")
 	return nil
-	// b.logger.Infof("rejecting just-works bluetooth pair attempt for %s, please initiate pairing from your mobile device settings", device)
-	// return &dbus.Error{Name: "org.bluez.Error.Rejected"}
+}
+
+func (b *basicAgent) DisplayPasskey(devicePath dbus.ObjectPath, passkey, entered uint32) *dbus.Error {
+	b.logger.Debugf("SMURF DisplayPasskey %s", passkey)
+	return nil
+}
+
+func (b *basicAgent) DisplayPinCode(devicePath dbus.ObjectPath, pincode string) *dbus.Error {
+	b.logger.Debugf("SMURF DisplayPinCode %s", pincode)
+	return nil
+}
+
+func (b *basicAgent) RequestConfirmation(devicePath dbus.ObjectPath, passkey uint32) *dbus.Error {
+	b.logger.Debug("SMURF RequestConfirmation")
+	return b.RequestAuthorization(devicePath)
+}
+
+func (b *basicAgent) RequestAuthorization(devicePath dbus.ObjectPath) *dbus.Error {
+	b.logger.Debugf("SMURF RequestAuthorization %+v", devicePath)
+	conn, _, err := getBluetoothDBus()
+	if err != nil {
+		b.logger.Error(err)
+		return errPairingRejected
+	}
+
+	remoteDev := conn.Object(BluezDBusService, devicePath)
+
+	bdaddr, err := remoteDev.GetProperty("org.bluez.Device1.Address")
+	if err != nil {
+		b.logger.Error(err)
+		return errPairingRejected
+	}
+
+	// bdaddrStr := strings.Trim(bdaddr.String(), "\"")
+	bdaddrStr := bdaddr.Value().(string)
+
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	b.logger.Infof("Bluetooth pairing request from: %s", bdaddrStr)
+	b.logger.Warnf("SMURF equal:%t, (%s), (%s)", bdaddrStr == b.trusted[0], bdaddrStr, b.trusted[0])
+
+	if !slices.Contains(b.trusted, bdaddrStr) {
+		b.logger.Errorf("Bluetooth device pairing rejected for %s, device address must be added via provisioning first.", bdaddrStr)
+		return errPairingRejected
+	}
+
+	if err := remoteDev.SetProperty("org.bluez.Device1.Trusted", true); err != nil {
+		b.logger.Error(errw.Wrapf(err, "trusting bluetooth device %s", bdaddrStr))
+	} else {
+		b.logger.Infof("Bluetooth device trusted: %s", bdaddrStr)
+	}
+
+	b.logger.Warnf("Bluetooth device paired: %s", bdaddrStr)
+
+	go func() {
+		b.logger.Warn("SMURF1")
+
+		var i int
+		for {
+			i++
+			ret, err := remoteDev.GetProperty("org.bluez.Device1.Paired")
+			if err != nil {
+				b.logger.Warn(errw.Wrapf(err, "cannot get paired status for bluetooth device %s", bdaddrStr))
+			}
+			if ret.Value().(bool) || i > 30 {
+				break
+			}
+			b.logger.Warnf("SMURF Value: %+v", ret.Value())
+			time.Sleep(time.Second * 1)
+		}
+		b.logger.Warn("SMURF2")
+		call := remoteDev.Call("org.bluez.Device1.ConnectProfile", 0, "00001115-0000-1000-8000-00805f9b34fb")
+		b.logger.Warn("SMURF3")
+		if err := call.Err; err != nil {
+			b.logger.Error(errw.Wrapf(err, "connecting bluetooth device %s", bdaddrStr))
+		}
+
+		b.logger.Infof("Connected bluetooth device for tethering: %s", bdaddrStr)
+	}()
+
+	return nil
 }
 
 func (n *Networking) enablePairing(deviceName string) error {
 	conn, adapter, err := getBluetoothDBus()
 	if err != nil {
+		return err
+	}
+
+	call := adapter.Call("org.bluez.Adapter1.StartDiscovery", 0)
+	if call.Err != nil {
 		return err
 	}
 
@@ -43,12 +135,13 @@ func (n *Networking) enablePairing(deviceName string) error {
 		return errw.Wrap(err, "adjusting discovery timeout")
 	}
 
-	if err := conn.Export(&basicAgent{logger: n.logger, conn: conn}, BluezAgentPath, BluezAgent); err != nil {
+	// SMURF remove trusted!
+	if err := conn.Export(&basicAgent{logger: n.logger, conn: conn, trusted: []string{"FC:41:16:BF:6D:98"}}, BluezAgentPath, BluezAgent); err != nil {
 		return errw.Wrap(err, "exporting custom agent object")
 	}
 
 	obj := conn.Object(BluezDBusService, "/org/bluez")
-	call := obj.Call("org.bluez.AgentManager1.RegisterAgent", 0, dbus.ObjectPath(BluezAgentPath), "NoInputNoOutput")
+	call = obj.Call("org.bluez.AgentManager1.RegisterAgent", 0, dbus.ObjectPath(BluezAgentPath), "DisplayYesNo")
 	if err := call.Err; err != nil {
 		return errw.Wrap(err, "registering custom agent")
 	}
@@ -68,8 +161,13 @@ func (n *Networking) disablePairing() error {
 		return err
 	}
 
+	call := adapter.Call("org.bluez.Adapter1.StopDiscovery", 0)
+	if call.Err != nil {
+		return err
+	}
+
 	obj := conn.Object(BluezDBusService, "/org/bluez")
-	call := obj.Call("org.bluez.AgentManager1.UnregisterAgent", 0, dbus.ObjectPath(BluezAgentPath))
+	call = obj.Call("org.bluez.AgentManager1.UnregisterAgent", 0, dbus.ObjectPath(BluezAgentPath))
 	if err := call.Err; err != nil {
 		n.logger.Warnf("failed to unregister a bluez agent: %v", err)
 	}
