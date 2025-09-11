045b90890037065a896ef120b3c64c46f8860664
Abe Winter <abe-winter@users.noreply.github.com>
2025-09-10 23:01:54 -0400

    deprovision script

---
+++ etc/deprovision.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# This script deprovisions an agent system, i.e. removes the viam.json file.
+# It will also remove bluetooth connections if you pass BLUETOOTH_MAC.
+# Todo: clean up wifi networks.
+
+set -eux
+
+sudo service viam-agent stop
+
+if [ -z "$BLUETOOTH_MAC" ]; then
+    bluetoothctl remove $BLUETOOTH_MAC || echo "didn't remove bluetooth device"
+    bluetoothctl power off
+    sudo nmcli c delete bluetooth@$BLUETOOTH_MAC || echo "didn't remove nmcli network"
+
+    sleep 2
+
+    bluetoothctl power on
+else
+    echo '$BLUETOOTH_MAC not set, skipping bluetooth cleanup'
+fi
+
+sudo rm -fv /etc/viam.json
+sudo service viam-agent start
+
