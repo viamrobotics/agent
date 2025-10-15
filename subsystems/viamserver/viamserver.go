2497b0543d1a3ad0e60c646a2515d0729436c8f2
Benji Rewis <benji.rewis@viam.com>
2025-10-15 12:09:03 -0400

    Go back to disallowing checks of restart_allowed on Windows

--- subsystems/viamserver/viamserver.go
+++ subsystems/viamserver/viamserver.go
@@ -288,8 +288,11 @@ func (s *viamServer) Property(ctx context.Context, property string) bool {
 
 	switch property {
 	case RestartPropertyRestartAllowed:
-		if !s.running {
-			// Assume agent can restart viamserver if the subsystem is not running.
+		if !s.running || runtime.GOOS == "windows" {
+			// Assume agent can restart viamserver if the subsystem is not running or we are on
+			// Windows.
+			//
+			// TODO(RSDK-12271): Allow checks of restart_allowed on Windows.
 			return true
 		}
 
