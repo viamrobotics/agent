9bb34ef3dc06125c76043c81952f221aae522229
danielbotros <daniel.botros@viam.com>
2026-01-29 12:11:50 -0500

    Deduplicate req logic

--- subsystems/viamserver/restart_properties.go
+++ subsystems/viamserver/restart_properties.go
@@ -117,93 +117,19 @@ func getAllLocalIPv4s() ([]string, error) {
 // Returns the value of the requested restart property (false if not determined) and any
 // encountered errors. Must be called with s.mu held, as makeTestURLs is called.
 func (s *viamServer) checkRestartProperty(ctx context.Context, rp restartProperty) (bool, error) {
-	urls, err := s.makeTestURLs(rp)
+	resp, err := s.fetchRestartStatus(ctx)
 	if err != nil {
 		return false, err
 	}
 
-	// Create a buffered channel for Result[bool] values. Sending to this channel should not
-	// block, as we'll only ever have len(urls) goroutines trying to send one value.
-	resultChan := make(chan mo.Result[bool], len(urls))
-
-	timeoutCtx, cancelFunc := context.WithTimeout(ctx, checkRestartPropertyTimeout)
-	defer cancelFunc()
-
-	// Disabling the cert verification because it doesn't work in offline mode (when
-	// connecting to localhost).
-	//nolint:gosec
-	client := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}}
-	defer func() {
-		// CloseIdleConnections at the end of the method to ensure that any goroutine created
-		// below does not leave an idle HTTP connection open to the server.
-		client.CloseIdleConnections()
-	}()
-
-	for _, url := range urls {
-		go func() {
-			s.logger.Debugf("Starting %s check for %s using %s", rp, SubsysName, url)
-
-			restartURL := url + restartURLSuffix
-
-			req, err := http.NewRequestWithContext(timeoutCtx, http.MethodGet, restartURL, nil)
-			if err != nil {
-				resultChan <- mo.Err[bool](
-					errw.Wrapf(err, "creating HTTP request for %s check for %s via %s",
-						rp, SubsysName, restartURL))
-				return
-			}
-
-			resp, err := client.Do(req)
-			if err != nil {
-				resultChan <- mo.Err[bool](errw.Wrapf(err, "sending HTTP request for %s check for %s via %s",
-					rp, SubsysName, restartURL))
-				return
-			}
-			defer func() {
-				goutils.UncheckedError(resp.Body.Close())
-			}()
-
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				// Interacting with older viam-server instances will result in a non-successful
-				// HTTP response status code, as the /restart_status endpoint will not be
-				// available.
-				resultChan <- mo.Errf[bool]("checking %s status via %s, got code: %d", SubsysName, restartURL, resp.StatusCode)
-				return
-			}
-
-			var restartStatusResponse RestartStatusResponse
-			if err = json.NewDecoder(resp.Body).Decode(&restartStatusResponse); err != nil {
-				resultChan <- mo.Err[bool](errw.Wrapf(err, "decoding HTTP response for %s check for %s via %s",
-					rp, SubsysName, restartURL))
-				return
-			}
-
-			switch rp {
-			case RestartPropertyRestartAllowed:
-				resultChan <- mo.Ok(restartStatusResponse.RestartAllowed)
-			case RestartPropertyDoesNotHandleNeedsRestart:
-				resultChan <- mo.Ok(restartStatusResponse.DoesNotHandleNeedsRestart)
-			}
-		}()
+	switch rp {
+	case RestartPropertyRestartAllowed:
+		return resp.RestartAllowed, nil
+	case RestartPropertyDoesNotHandleNeedsRestart:
+		return resp.DoesNotHandleNeedsRestart, nil
+	default:
+		return false, errw.Errorf("unknown restart property: %s", rp)
 	}
-
-	var combinedErr error
-	for range urls {
-		select {
-		case result := <-resultChan:
-			property, err := result.Get()
-			if err != nil {
-				combinedErr = errors.Join(combinedErr, err)
-			} else {
-				// We assume below that the first test URL through which we encountered the feature's
-				// value represents the actual value.
-				return property, nil
-			}
-		case <-timeoutCtx.Done():
-			return false, errors.Join(combinedErr, ctx.Err())
-		}
-	}
-	return false, combinedErr
 }
 
 // fetchRestartStatus fetches the full RestartStatusResponse from viam-server.
