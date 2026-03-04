1adce2385f999dfdf5205f9a48d9399d223dc841
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2026-03-04 13:30:33 -0500

    RSDK-13169 Add [un]install.sh and `--install` support for launchd/MacOS (#199)
    Co-authored-by: Josh Matthews <josh@jmatth.com>

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,15 @@ if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
 fi
 
 if [ "$(id -u)" -ne 0 ]; then
-	echo "This install script must be run as root. Try running via sudo."
+	echo "This uninstall script must be run as root. Try running via sudo."
+	exit 1
+fi
+
+OS=$(uname -s)
+if ! [ "$OS" = "Linux" -o "$OS" = "Darwin" ]; then
+	echo "This uninstall script can only be run on Linux or MacOS."
+	echo
+	echo "Please see https://docs.viam.com/manage/reference/viam-agent/manage-viam-agent/ to uninstall on other platforms."
 	exit 1
 fi
 
@@ -23,23 +31,35 @@ if [ -z "$FORCE" ]; then
 	fi
 fi
 
-# system services
-systemctl disable --now viam-agent
-systemctl disable --now viam-server
-rm -v /etc/systemd/system/viam-agent.service /usr/local/lib/systemd/system/viam-agent.service /etc/systemd/system/viam-server.service
-systemctl daemon-reload
+if [ $OS = "Linux" ]; then
+	# systemd services
+	systemctl disable --now viam-agent
+	systemctl disable --now viam-server
+	rm -v /etc/systemd/system/viam-agent.service /usr/local/lib/systemd/system/viam-agent.service /etc/systemd/system/viam-server.service
+	systemctl daemon-reload
 
-# previous appimage installs
-rm -v /usr/local/bin/viam-server
+	# previous appimage installs
+	rm -v /usr/local/bin/viam-server
 
-# configs
-rm -v /etc/viam.json /etc/viam-provisioning.json /etc/viam-defaults.json
+	# agent-provisioning customization
+	rm -v /etc/NetworkManager/conf.d/80-viam.conf /etc/NetworkManager/dnsmasq-shared.d/80-viam.conf
 
-# root/viamdir
-rm -vr /root/.viam/
+	# provisioning and default configs
+	rm -v /etc/viam-provisioning.json /etc/viam-defaults.json
+
+	# root/viamdir
+	rm -vr /root/.viam/
+else
+	# launchd daemon
+	launchctl bootout system/com.viam.agent
+	rm -v /Library/LaunchDaemons/com.viam.agent.plist
+
+	# root/viamdir
+	rm -vr /var/root/.viam/
+fi
+
+# config
+rm -v /etc/viam.json
 
 # agent home
 rm -vr /opt/viam/
-
-# agent-provisioning customization
-rm -v /etc/NetworkManager/conf.d/80-viam.conf /etc/NetworkManager/dnsmasq-shared.d/80-viam.conf
