650c207c05c6baea7cac61c4ddc337be306fab7c
danielbotros <daniel.botros@viam.com>
2026-03-09 16:53:40 -0400

    Lint 2

--- version_control.go
+++ version_control.go
@@ -346,7 +346,7 @@ func (c *VersionCache) UpdateBinary(ctx context.Context, binary string) (bool, e
 			if runtime.GOOS == "windows" {
 				expectedMimes = []string{"application/vnd.microsoft.portable-executable"}
 			}
-
+			//nolint:goconst
 			if runtime.GOOS == "darwin" {
 				expectedMimes = []string{"application/x-mach-binary"}
 			}
@@ -408,6 +408,7 @@ func (c *VersionCache) getProtectedFilesAndCleanVersions(ctx context.Context, ma
 	// add protection for the current symlinked binaries
 	for _, path := range []string{"viam-agent", "viam-server"} {
 		if runtime.GOOS == "windows" {
+			//nolint:goconst
 			path += ".exe"
 		}
 
