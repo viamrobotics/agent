a66db3cbd48bf0e552aa6734df06f8830afaa2f8
Evan Dorsky <evan.dorsky@icloud.com>
2026-06-30 16:51:55 -0400

    Config validation

--- agent_serial_test.go
+++ agent_serial_test.go
@@ -141,12 +141,17 @@ func InitializeSuite(t *testing.T) func(*godog.TestSuiteContext) {
 			// Set to INFO to see the commands being sent to the terminal, DEBUG to
 			// see the commands + any output they produce.
 			logger.SetLevel(logging.WARN)
+
+			// Validate the config before attempting to do anything
+			if err := validateTestConfig(cfg); err != nil {
+				panic(fmt.Errorf("login failed: %w", err))
+			}
+
+			// Log in
 			serialClient = serialcontrol.Connect(
 				logger,
 				cfg.Serial.Path.OrElse("/dev/ttyUSB0"),
 			).MustGet()
-
-			// Log in
 			if err := serialClient.Login(cfg.Serial.User, cfg.Serial.Pass); err != nil {
 				serialClient.Close()
 				panic(fmt.Errorf("login failed: %w", err))
@@ -174,16 +179,12 @@ func InitializeSuite(t *testing.T) func(*godog.TestSuiteContext) {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
 			defer cancel()
 			// now that everything is set up, log the test conditions
-			if cfg.Versions.Test != "" {
-				concrete, err := resolveVersionSpec(ctx, cfg.Versions.Test)
-				concreteTestVersion = concrete
-				if err != nil {
-					panic(fmt.Errorf("resolving install version %q: %w", cfg.Versions.Test, err))
-				}
-				logger.Infof("Version under test: %s\n", concreteTestVersion)
-			} else {
-				panic(fmt.Errorf("viam_agent_test in agent-test.toml cannot be empty string"))
+			concrete, err := resolveVersionSpec(ctx, cfg.Versions.Test)
+			concreteTestVersion = concrete
+			if err != nil {
+				panic(fmt.Errorf("resolving install version %q: %w", cfg.Versions.Test, err))
 			}
+			logger.Infof("Version under test: %s\n", concreteTestVersion)
 
 			centerPrint := func(msg string, width int) {
 				padLenTotal := width - len(msg)
@@ -311,6 +312,41 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 		}
 		return ctx, nil
 	})
+}
+
+func validateTestConfig(cfg config) error {
+	// check for < or > in fields that could never contain them,
+	// that's a good indicator that default values were kept
+	// don't check any user-provided fields like passwords since we don't want
+	// any false positives
+	checkStr := cfg.APIKey +
+		cfg.APIKeyID +
+		cfg.Versions.Old +
+		cfg.Versions.Stable +
+		cfg.Versions.Test +
+		cfg.Versions.ViamServerOld +
+		cfg.Versions.ViamServerStable
+
+	if strings.Contains(checkStr, "<") || strings.Contains(checkStr, ">") {
+		return errors.New("invalid config: some fields contain placeholder characters. Please fill out all fields in agent-test.toml")
+	}
+	if cfg.Versions.Test != "" {
+		return errors.New("viam_agent_test in agent-test.toml cannot be empty string")
+	}
+	if cfg.Versions.Old == "" {
+		return errors.New("must set viam_agent_old in config")
+	}
+	if cfg.Versions.ViamServerOld == "" {
+		return errors.New("must set viam_server_old in config")
+	}
+	if cfg.Versions.Old == "" {
+		return errors.New("must set viam_agent_old in config")
+	}
+	if cfg.Versions.ViamServerOld == "" {
+		return errors.New("must set viam_server_old in config")
+	}
+
+	return nil
 }
 
 func dialApp(ctx context.Context, logger logging.Logger, address string, keyID, key string) mo.Result[apppb.AppServiceClient] {
@@ -1181,9 +1217,6 @@ func applyAgentVersionPin(ctx context.Context, version string) (context.Context,
 }
 
 func applyAgentURLPin(ctx context.Context) (context.Context, error) {
-	if cfg.Versions.Old == "" {
-		return ctx, errors.New("must set viam_agent_old in config")
-	}
 	return applyVersionPin(ctx, gcsURL("viam-agent", cfg.Versions.Old), "agent", "version_control", "agent")
 }
 
@@ -1192,9 +1225,6 @@ func applyAgentFilePin(ctx context.Context) (context.Context, error) {
 }
 
 func applyAgentViamServerBinaryPin(ctx context.Context) (context.Context, error) {
-	if cfg.Versions.ViamServerOld == "" {
-		return ctx, errors.New("must set viam_server_old in config")
-	}
 	return applyVersionPin(ctx, gcsURL("viam-server", cfg.Versions.ViamServerOld), "agent", "version_control", "agent")
 }
 
@@ -1203,9 +1233,6 @@ func testAgentRejectedInvalidBinary(ctx context.Context) (context.Context, error
 }
 
 func downloadOldAgentBinary(ctx context.Context) (context.Context, error) {
-	if cfg.Versions.Old == "" {
-		return ctx, errors.New("must set viam_agent_old in config")
-	}
 	return ctx, serialClient.DownloadToDevice(gcsURL("viam-agent", cfg.Versions.Old), "/tmp/viam-agent-old").Error()
 }
 
@@ -1243,9 +1270,6 @@ func applyViamServerVersionPin(ctx context.Context, version string) (context.Con
 }
 
 func applyViamServerURLPin(ctx context.Context) (context.Context, error) {
-	if cfg.Versions.ViamServerOld == "" {
-		return ctx, errors.New("must set viam_server_old in config")
-	}
 	return applyVersionPin(ctx, gcsURL("viam-server", cfg.Versions.ViamServerOld), "agent", "version_control", "viam-server")
 }
 
@@ -1254,9 +1278,6 @@ func applyViamServerFilePin(ctx context.Context) (context.Context, error) {
 }
 
 func downloadOldViamServerBinary(ctx context.Context) (context.Context, error) {
-	if cfg.Versions.ViamServerOld == "" {
-		return ctx, errors.New("must set viam_server_old in config")
-	}
 	return ctx, serialClient.DownloadToDevice(gcsURL("viam-server", cfg.Versions.ViamServerOld), "/tmp/viam-server-old").Error()
 }
 
